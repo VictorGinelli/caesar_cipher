586876bc96f30b6603b558115474271715418c99
Victor Ginelli <vginelli18@cmc.edu>
2018-02-03 15:21:43 -0800

    Caesar_Cipher

--- caesar_cipher_spec.rb
+++
@@ -1,7 +0,0 @@
-require caesar_cipher
-
-describe "Caesar Cipher" do
-  describe "caesar_cipher" do
-    it "should print the input coded" do
-      expect(caeser_cipher("hello", 3)).to eq("khoor")
-    end
